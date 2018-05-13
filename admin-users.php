46e5ed3e0dc9c744c73f112f910d1d3d7db0170f
johnnydoni <inods@hotmail.com>
2018-05-13 12:18:20 -0300

    Admin - User - Search And Pagination

--- admin-users.php
+++ admin-users.php
@@ -5,10 +5,32 @@ use \Hcode\Model\User;
 
 $app->get('/admin/users', function() {
 	User::verifyLogin();
-	$user = User::listAll();
+
+    $search = (isset($_GET["search"])) ? $_GET["search"] : "";
+    $page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
+    if ($search != "") {
+        $pagination = User::getPageSearch($search, $page);
+    } else {
+        $pagination = User::getPage($page);
+    }
+
+    $pages = [];
+    for ($x=0; $x < $pagination["pages"]; $x++) { 
+        array_push($pages, [
+            "href"=>"/admin/users?" . http_build_query([
+                "page"=>$x+1,
+                "search"=>$search
+            ]),
+            "text"=>$x+1
+        ]);
+    }
+
 	$page = new PageAdmin();
 	$page->setTpl("users", array(
-		"users"=>$user
+		"users"=>$pagination["data"],
+        "search"=>$search,
+        "pages"=>$pages
 	));
 });
 
