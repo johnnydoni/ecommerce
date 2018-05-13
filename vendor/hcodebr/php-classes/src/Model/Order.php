20d987dd7819fecbf78f3ad8b11602f17befd03b
johnnydoni <inods@hotmail.com>
2018-05-13 11:10:37 -0300

    Admin - Pedidos

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -4,8 +4,12 @@ namespace Hcode\Model;
 
 use \Hcode\Db\Sql;
 use \Hcode\Model;
+use \Hcode\Model\Cart;
 
 class Order extends Model {
+
+	const SESSION_ERROR = "OrderError";
+	const SESSION_SUCCESS = "OrderSuccess";
 
 	public function save() {
 		$sql = new Sql();
@@ -41,6 +45,62 @@ class Order extends Model {
 			$this->setData($results[0]);
 		}
 	}
+
+	public static function listAll() {
+		$sql = new Sql();
+		return $sql->select("
+			SELECT * 
+			FROM tb_orders a
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			ORDER BY a.dtregister DESC
+		");
+	}
+
+	public function delete() {
+		$sql = new Sql();
+		$sql->query("DELETE FROM tb_orders WHERE idorder = :idorder", [
+			":idorder"=>$this->getidorder()
+		]);
+	}
+
+	public function getCard():Cart {
+		$cart = new Cart();
+		$cart->get((int)$this->getidcart());
+		return $cart;
+	}
+
+	public static function setMsgError($msg) {
+		$_SESSION[Order::SESSION_ERROR] = $msg;
+	}
+
+	public static function getMsgError() {
+		$msg = (isset($_SESSION[Order::SESSION_ERROR]) && $_SESSION[Order::SESSION_ERROR]) ? $_SESSION[Order::SESSION_ERROR]  : "";
+		Order::clearMsgError();
+		return $msg;
+	}
+
+	public static function clearMsgError() {
+		$_SESSION[Order::SESSION_ERROR] = NULL;
+	}
+
+	public static function setMsgSuccess($msg) {
+		$_SESSION[Order::SESSION_SUCCESS] = $msg;
+	}
+
+	public static function getMsgSuccess() {
+		$msg = (isset($_SESSION[Order::SESSION_SUCCESS]) && $_SESSION[Order::SESSION_SUCCESS]) ? $_SESSION[Order::SESSION_SUCCESS]  : "";
+		Order::clearMsgSuccess();
+		return $msg;
+	}
+
+	public static function clearMsgSuccess() {
+		$_SESSION[Order::SESSION_SUCCESS] = NULL;
+	}	
+
 }
 
  ?>
